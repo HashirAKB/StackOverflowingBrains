a3cfdf9a3912f8f54e21f15f46048a1f177943ad
NETAPP\hashirah <hashirah@netapp.com>
2023-07-28 16:44:14 +0530

    added code and notes

---
+++ code/state_and_props/src/StateAndProps.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter(){
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    return(
+        <div>
+            <h2>Count = {count}</h2>
+            <button onClick={increment}>Increment Count</button>
+            <button onClick={decrement}>Decrement Count</button>
+
+        </div>
+    );
+
+}
+export default Counter;
